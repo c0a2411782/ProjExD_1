94e349c32db63d479ea6413d8b65caad82650879
tatsuki <c0a2411782@edu.teu.ac.jp>
2025-11-18 18:01:44 +0900

    演習：問2

--- pygame_image.py
+++ pygame_image.py
@@ -21,22 +21,23 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
 
         key_lst = pg.key.get_pressed()
-        kk_rct.move_ip(-2, 0)
+        a, b = -2, 0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -2)
+            b -= 2
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, 2)
+            b += 2
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-2, 0)
+            a -= 2
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(2, 0)
+            a += 2
+
+        kk_rct.move_ip(a, b)
 
         screen.blit(kk_img, kk_rct)
 
