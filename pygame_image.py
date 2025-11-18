202aa4e02371b454919590fea44a0690aad9fb5b
tatsuki <c0a2411782@edu.teu.ac.jp>
2025-11-18 17:59:35 +0900

    演習：問１

--- pygame_image.py
+++ pygame_image.py
@@ -8,10 +8,8 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock = pg.time.Clock()
-
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img, True, False)
-
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_rct = kk_img.get_rect()
@@ -24,29 +22,26 @@ def main():
             if event.type == pg.QUIT:
                 return
 
-        # 背景スクロール
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
 
-        # 移動処理
         key_lst = pg.key.get_pressed()
-
+        kk_rct.move_ip(-2, 0)
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
+            kk_rct.move_ip(0, -2)
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, 1)
+            kk_rct.move_ip(0, 2)
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+            kk_rct.move_ip(-2, 0)
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(1, 0)
+            kk_rct.move_ip(2, 0)
 
-        # こうかとん描画
         screen.blit(kk_img, kk_rct)
 
         pg.display.update()
-        tmr += 5
+        tmr += 2
         clock.tick(200)
 
 
